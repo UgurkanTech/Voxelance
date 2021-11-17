cd934ca68b9af0631df5bb94a29dd61868640c68
UgurkansTR <ugurkanstr@gmail.com>
2021-11-18 02:10:43 +0300

    before plug

--- Source/Voxelance/Voxelance.Build.cs
+++ Source/Voxelance/Voxelance.Build.cs
@@ -8,7 +8,7 @@ public class Voxelance : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "FastNoiseGenerator", "FastNoise" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
